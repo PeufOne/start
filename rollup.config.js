3cb8f223f6c5310c57f407fa961fc9c738493d40
PeufOne <jonas.voisard@gmail.com>
2019-12-25 16:51:54 +0100

    Remove font awasome

--- rollup.config.js
+++ rollup.config.js
@@ -3,50 +3,50 @@ import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import livereload from 'rollup-plugin-livereload'
 import { terser } from 'rollup-plugin-terser'
-import scss from 'rollup-plugin-scss'
-import multiEntry from "rollup-plugin-multi-entry"
-import url from 'rollup-plugin-url'
+import postcss from 'rollup-plugin-postcss'
 
 const production = !process.env.ROLLUP_WATCH
 
-function getInput(name) {
-	return [
-		`src/${name}.js`,
-		'node_modules/w3-css',
-		'node_modules/@fortawesome/fontawesome-free/css/all.css',
-		'node_modules/@fortawesome/fontawesome-free/webfonts/fa-brands-400.woff2',
-		'node_modules/@fortawesome/fontawesome-free/webfonts/fa-regular-400.woff',
-		'node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.woff2'
-	]
-}
 function getOutput(name) {
 	return {
-		sourcemap: true,
+		sourcemap: !production,
 		format: 'iife',
 		name: 'app',
 		file: `public/assets/${name}.js`
 	}
 }
+
 const plugins = [
-		svelte({dev: !production}),
-		multiEntry(),
-		scss(),
-		url({
-			include: ['**/*.woff2', '**/*.woff'],
-			publicFiles: './public/assets',
-			fileName: '[dirname][name][extname]'
+		svelte({
+			dev: !production,
+			emitCss: true
 		}),
-		resolve(),
+		resolve({
+			browser: true
+		}),
 		commonjs(),
+		postcss({
+			extract: true,
+			minimize: true,
+			use: [
+			  ['sass', {
+				includePaths: [
+				  './src/theme',
+				  './node_modules'
+				]
+			  }]
+			]
+		}),
 		!production && livereload('public'),
 		production && terser()
-
 	]
 
 
-export default [{
-	input: getInput('index'),
-	output: getOutput('index'),
+export default [
+{
+	input: 'src/main.js',
+	output: getOutput('main'),
 	plugins,
 	watch: {clearScreen: false}
-}]
+},
+]
